6548d6d4303395a7e82bb13a841d5749664cebd0
Anderson Silva <nutriandersonsilva@gmail.com>
2022-09-22 20:39:32 -0300

    Atividade Variáveis1

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia01-introducao-JavaScript/scripts.js
@@ -0,0 +1,16 @@
+const myName = 'Anderson';
+const birthCity = 'Sumare';
+let birthYear = 1995;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = Campinas
+console.log(birthCity)
+
+
+/* Errou aconteceu pois birthCity é uma variavel constante, não pode alterar seu valor. */
