00eaa2883ff0ce7d61c09828177448107304128e
Anderson Silva <nutriandersonsilva@gmail.com>
2022-09-22 21:01:28 -0300

    atividade 2

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia01-introducao-JavaScript/scripts2.js
@@ -0,0 +1,9 @@
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+const perimete = base + heigth + area;
+
+console.log(base);
+console.log(heigth);
+console.log(area);
+console.log(perimete);
