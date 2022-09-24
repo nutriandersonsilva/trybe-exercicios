c7fb74ef075d881a00fa2953aacd0a338e4a2484
Anderson Silva <nutriandersonsilva@gmail.com>
2022-09-24 12:05:13 -0300

    atividades de fixar

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia02-javaScript-Array-e-loop-For/arrays-fixar.js
@@ -0,0 +1,18 @@
+/* Obtenha o valor “Serviços” do array menu: */
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1] ;
+
+console.log(menuServices);
+console.log('')
+
+/* Procure o índice do valor “Portfólio” do array menu: */
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu[2];
+
+console.log(indexOfPortfolio);
+console.log('')
+/* Adicione o valor “Contato” no final do array menu: */
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+    menu2.push('Contato')
+console.log(menu2);
+
